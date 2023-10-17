53398fb3ec24689095c54beb834b7dc4efdfdd05
cryptogeek0 <148174876+cryptogeek0@users.noreply.github.com>
2023-10-17 14:33:01 -0500

    Update vast.sh
    deleted sleep

--- vast.sh
+++ vast.sh
@@ -3,7 +3,7 @@
 # Update package list and install necessary packages
 
 sudo apt update
-sleep 15s
+
 echo "---------------------------"
 
 sudo apt -y install ocl-icd-opencl-dev
@@ -41,23 +41,22 @@ chmod +x build.sh
 echo "---------------------------"
 
 ./build.sh
-sleep 15s
+
 echo "---------------------------"
 
 # Update the configuration file
 sed -i 's/account = 0x24691e54afafe2416a8252097c9ca67557271475/account = 0x73128CfA687501a43aE310320d03967070396514/g' config.conf
-sleep 15s
+
 echo "---------------------------"
 
 # Install Python requirements
 sudo pip install -U -r requirements.txt
-sleep 15s
+
 echo "---------------------------"
 
 sudo nohup python3 miner.py --gpu=true > miner.log 2>&1 &
-sleep 15s
+
 echo "---------------------------"
 
-sudo nohup ./xengpuminer > xengpuminer.log 2>&1 &
-sleep 15s
+sudo nohup ./xengpuminer > xengpuminer.log 2>&1 
 echo "---------------------------"
