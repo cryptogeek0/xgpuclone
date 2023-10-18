ea2d2058a1d3793e13755e0250d3364d23a6f5de
cryptogeek0 <148174876+cryptogeek0@users.noreply.github.com>
2023-10-18 00:32:47 -0500

    Update vast.sh

--- vast.sh
+++ vast.sh
@@ -59,6 +59,6 @@ sudo nohup python3 miner.py --gpu=true > miner.log 2>&1 &
 
 echo "---------------------------"
 
-sudo nohup ./xengpuminer > xengpuminer.log 2>&1
+sudo nohup ./xengpuminer > xengpuminer.log -0 2>&1
 # sudo nohup xengpuminer > xengpuminer.log 2>&1
 echo "---------------------------"
